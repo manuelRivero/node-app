4f9d578b568c75b7c9cd96d1f2860f70ea7e7cb4
manuel rivero <manuel.enrique.r.v@gmail.com>
2019-11-17 18:15:26 -0400

    refactor: product model with Sequelize

--- models/product.js
+++ models/product.js
@@ -1,61 +1,31 @@
-fs = require('fs');
-path = require('path')
-const db = require('../util/database');
-cart = require('./cart');
+const Sequelize = require('sequelize');
 
-const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
+const sequelize = require('../util/database');
 
-const getAllProducts = cb => {
-    fs.readFile(p, (err, data)=>{
-        if(err){
-            return cb([]);
-        }
-        
-        cb(JSON.parse(data))
-    })
-}
-
-module.exports = class Product {
-    constructor(title, price, imgUrl, description){
-        this.title = title;
-        this.price = price;
-        this.imgUrl = imgUrl;
-        this.description = description;
-    }
-
-    save(){
-        return db.execute('INSERT INTO products (title, price, imgUrl, description) VALUES (?, ?, ?, ?)',
-        [this.title, this.price, this.imgUrl, this.description]);
-    }
-
-    static fetchAll(){
-        return db.execute('SELECT * from products');
+const products = sequelize.define('products',{
+    id:{
+        type:Sequelize.INTEGER,
+        autoIntrement: true,
+        allowNull: false,
+        primaryKey:true
+    },
+    title:{
+        type:Sequelize.INTEGER,
+        allowNull: false,
+    },
+    price:{
+        type:Sequelize.DOUBLE,
+        allowNull: false,
+    },
+    imgUrl:{
+        type:Sequelize.STRING,
+        allowNull: false,
+    },
+    description:{
+        type:Sequelize.STRING,
+        allowNull: false,
 
     }
+})
 
-    static deleteById(id){
-        return db.execute('SELECT * FROM productos WHERE idproducts === ?', [id])
-    }
-
-    static update(product, cb){
-        let {id}=product;
-        getAllProducts( products => {
-            let productId = products.findIndex(prod => prod.id === parseInt(id) )
-            products[productId] = {...product, id:parseInt(id)};
-            
-            fs.writeFile(p, JSON.stringify(products), (err)=>{
-                if(!err){
-                    return cb();
-                }
-                console.log(err)
-            })
-            
-            
-        } )
-    }
-
-    static getById(id){
-        return db.execute('SELECT * FROM products WHERE id = ?', [id])
-    }
-}
-
+module.exports = products;
