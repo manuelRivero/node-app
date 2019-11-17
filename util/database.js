56bd3af6787d319eb8e73b6b879ceff30f8b85f2
manuel rivero <manuel.enrique.r.v@gmail.com>
2019-11-17 17:27:14 -0400

    add sequelize and connect to database

--- util/database.js
+++ util/database.js
@@ -1,11 +1,7 @@
-const mysql = require('mysql2');
-
-const pool = mysql.createPool({
-    host:'localhost',
-    user:'root',
-    database:'node-course',
-    password:'manuel12345'
-})
 
 
-module.exports = pool.promise()
+const Sequelize = require('sequelize')
+
+const sequelize = new Sequelize('node-course','root','manuel12345', {dialect: 'mysql', host: 'localhost'});
+
+module.exports = sequelize;
