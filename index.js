e84556a6715abd5516416d76dc52b87575cf62d5
manuel rivero <manuel.enrique.r.v@gmail.com>
2019-11-17 18:48:42 -0400

    add: product table creation if not exist

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 
-const express = require('express')
+const express = require('express');
 
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const path = require('path');
 
 
-const adminRouter = require('./routes/admind')
-const shopRouter = require('./routes/shop')
+const adminRouter = require('./routes/admind');
+const shopRouter = require('./routes/shop');
+
+const sequelize = require('./util/database');
 
 const app = express();
 
-const notFound = require('./controllers/error')
+const notFound = require('./controllers/error');
 app.set('view engine', 'pug');
 app.set('views', 'views')
 
@@ -19,10 +21,16 @@ app.use(bodyParser.urlencoded({extended:false}))
 app.use(adminRouter);
 app.use(shopRouter);
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use( notFound.notFound)
+app.use( notFound.notFound);
 
-app.listen(5000);
+sequelize.sync()
+.then( res =>{
+    app.listen(5000);
+})
+.catch( err => console.log(err))
+
+
 
 
