c9c14ee8b144701e50806221528fb6df2f88ec33
manuel rivero <manuel.enrique.r.v@gmail.com>
2020-02-04 22:49:22 -0300

    [refactor] add user class with mongodb

--- models/user.js
+++ models/user.js
@@ -1,17 +1,24 @@
-const Sequelize = require("sequelize");
-const sequelize = require('./../util/database');
+const {ObjectId} = require(mongodb)
+const {getDB }= require('./../util/database')
 
+class User {
+    constructor (name, email, id = null){
+        this.name=name;
+        this.email=email;
+        this.id = 
+    }
 
-const user = sequelize.define('user', 
-{
-    id:{
-        type:Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey:true
-    },
-    name: Sequelize.STRING,
-    email: Sequelize.STRING
-});
+    static save(){
+
+        let db=getDB()
+        return db.colletion('users').insertOne(this)
+    }
+
+    static findById(id){
+        
+        let db=getDB()
+        return db.colletion('users').findOne({_id: new ObjectId(id)})
+    }
+}
 
 module.exports = user;
